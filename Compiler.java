cbe81e5b74047b6892a6fd9021176c00c34db4b2
Gabriel Lopes <glf7655@gmail.com>
2021-10-24 19:49:59 -0300

    updates

--- Compiler.java
+++ Compiler.java
@@ -293,9 +293,9 @@ class Symbol {
 
 class ExpressionReturn {
     Type type;
-    int address;//TODO geração de código
+    long address;//TODO geração de código
 
-    ExpressionReturn(Type type, int address){
+    ExpressionReturn(Type type, long address){
         this.type = type;
         this.address = address;
     }
